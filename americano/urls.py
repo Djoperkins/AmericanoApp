f285bcb5654d11882053c581c9ea74ae81b0e41a
Djoperkins <perkinsdjo@gmail.com>
2021-06-30 23:28:26 +0200

    Modify display for photos and bio

--- americano/urls.py
+++ americano/urls.py
@@ -21,3 +21,5 @@ urlpatterns = [
     path('blog/', include('blog.urls')),
     path('admin/', admin.site.urls),
 ]
+# if settings.DEBUG:
+#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
